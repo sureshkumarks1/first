f239014fbe865c5faed8b453f29b65831687ca6d
SURESH KUMAR K S <sureshkumarks1@gmail.com>
2024-05-04 16:28:08 +0530

    added the zoom option to product image

--- public/user/js/slider.js
+++ public/user/js/slider.js
@@ -1,3 +1,16 @@
+
+$(function (){
+    $(".xzoom, .xzoom-gallery").xzoom({
+        zoomWidth:300,
+        tint:"#333",
+        Xoffset:15,
+    });
+
+    $("#one").click();
+});
+
+
+/*
 const imgs = document.querySelectorAll('.img-select a');
 const imgBtns = [...imgs];
 let imgId = 1;
@@ -6,9 +19,13 @@ imgBtns.forEach((imgItem) => {
     imgItem.addEventListener('click', (event) => {
         event.preventDefault();
         imgId = imgItem.dataset.id;
+        console.log(imgId)
         slideImage();
     });
 });
+///////////////////////////////
+
+///////////////////////////////
 
 function slideImage(){
     const displayWidth = document.querySelector('.img-showcase img:first-child').clientWidth;
@@ -17,17 +34,32 @@ function slideImage(){
 }
 
 window.addEventListener('resize', slideImage);
+*/
 
-const options1 = {
-    width: 100,
-    zoomWidth: 100,
-    offset: {vertical: 0, horizontal: 15}
-};
 
-// If the width and height of the image are not known or to adjust the image to the container of it
-const options2 = {
-    fillContainer: true,
-    offset: {vertical: 0, horizontal: 15}
-};
+            // let options = {
+            //     zoom: {
+            //         width: 100,
+            //         height: 100,
+            //         zIndex: 600
+            //     },
+            //     overlay: {
+            //         opacity: 0.65,
+            //         zIndex: 500,
+            //         backgroundColor: '#000000',
+            //         fade: true
+            //     },
+            //     detail: {
+            //         zIndex: 600,
+            //         margin: {
+            //             top: 0,
+            //             left: 10
+            //         },
+            //         fade: true
+            //     }
+            // };
+            
+                // $('#imgZoom').mooZoom(options);
+               
 
-new ImageZoom(document.getElementById("img-container"), options2);
+               
